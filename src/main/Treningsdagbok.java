6f7a756f30fcab691e36dd314e3357082d4029c0
mathilie <mlien1995@gmail.com>
2017-03-16 18:58:30 +0100

    gitignore

---
+++ src/main/Treningsdagbok.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Treningsdagbok {
+
+	Scanner s = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	private void mainMenu(){
+		System.out.println("Hva vil du gjøre?\n"
+				+ "");
+	}
+	
+}
